a555957f8d56fc079b1455376129051043566688
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-10-17 16:09:37 +0600

    added apartment related apis

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
-
 const app = express();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const port = process.env.PORT || 5000;
 
-// Middleware
+//middleware =======
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb + srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wi4y4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wi4y4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-serverApi: {
-  version: ServerApiVersion.v1,
-  strict: true,
-  deprecationErrors: true,
-},
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-try {
-  await client.connect();
-  console.log("✅ Connected to MongoDB!");
+  try {
+    const appartmants_collection = client.db("Lux-tower").collection("appartments");
 
-  const collection = client.db("yourDatabaseName").collection("yourCollectionName");
-  
-  app.get("/data", async (req, res) => {
-    const result = await collection.find().toArray();
-    res.send(result);
-  });
+    //--------------------------- Apartment Related APIs -------------------------------
+    app.get("/appartmants", async (req, res) => {
+      const result = await appartmants_collection.find().toArray();
+      res.send(result);
+    });
 
-}
-catch (error) {
-  console.error("❌ MongoDB connection error:", error);
-}
-}
+    app.get("/appartmantsPasition", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result = await appartmants_collection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
 
-//run
+    app.get("/appartmentlength", async (req, res) => {
+      const result = await appartmants_collection.find().toArray();
+      res.send({ count: result.length });
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("✅ Connected to MongoDB!");
+  }
+  finally {
+  }
+}
 run().catch(console.dir);
 
+//default route
 app.get("/", (req, res) => {
-res.send("Server is up and running!");
+  res.send("Server is up and running!");
 });
 
+//listing port
 app.listen(port, () => {
-console.log(`Server is listening on port ${port}`);
+  console.log(`server port is ${port}`);
 });
